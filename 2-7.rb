4082736564593ceba306eaf971b8042ead813495
大丸　裕喜 <marumaru@oomaruhirokinoMacBook-Pro.local>
2020-05-03 19:42:32 +0900

    2nd

---
+++ 2-7.rb
@@ -0,0 +1,20 @@
+apple = 110
+
+if apple >=100
+  puts "りんごの値段は１００円以上です"
+end
+
+if  apple <= 100
+  puts "りんごの値段は１００円以下です"
+end
+
+total_price = 50
+if total_price > 100
+  puts "みかんを購入あまりあり"
+
+elsif total_price == 100
+  puts "みかんを購入余りなし"
+else total_price < 100
+  puts "みかんを購入することが出来ません"
+
+end
---
+++ 2-8.rb
@@ -0,0 +1,7 @@
+puts "計算をはじめます"
+puts "２つの数字を入力してください"
+a=gets.to_i
+b=gets.to_i
+puts "出力します"
+puts "a*b=#{a*b}"
+puts "終了します"
