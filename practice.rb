a2948d4f384237ddbdabe42c4b7cf1746c177b81
大丸　裕喜 <marumaru@oomaruhirokinoMacBook-Pro.local>
2020-05-03 12:31:29 +0900

    finish 2-6

--- practice.rb
+++ practice.rb
@@ -1,5 +1,7 @@
-puts "hello"
-puts "webcamp".length
-puts "webcamp".reverse
-puts "webcampでプログラミング学習".include?("webcamp")
-puts "webcamp".upcase
+name=["perl","ruby","python","swift"]
+puts name[1]
+tall={"太郎"=>185,"次郎"=>170,"花子"=>150}
+puts tall ["太郎"]
+
+subject=["算数","国語","理科","社会"]
+puts subject[2]
